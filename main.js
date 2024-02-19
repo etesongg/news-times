83612d138605735150818921f416fb10c820bf85
etesong014 <etesong0114@gmail.com>
2024-02-19 23:39:22 +0900

    뉴스 타임즈 만들어보기 - news api를 통한 기본 render - 카테고리별 검색 기능

---
+++ main.js
@@ -0,0 +1,52 @@
+ const API_KEY = `30202e1a80e04c63b90325f9ec26c81a`; // newsapi에서 제공하는 기본키임 개인키 아님
+let newsList = []
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) => 
+    menu.addEventListener("click", (event) => getNewsCategory(event))
+);
+
+const getLatestNews = async() => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const response = await fetch(url); // await 없으면 pending(미뤄짐, 대기상태) 뜸
+    const data = await response.json();
+    newsList = data.articles
+    render()
+};
+
+const getNewsCategory = async(evnet) => {
+    const gatcgory = evnet.target.textContent.toLowerCase()
+    const url = new URL(`https://newsapi.org/v2/top-headlines?category=${gatcgory}&country=us&apiKey=${API_KEY}`);
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles
+    render();
+}
+
+const render = ()=>{
+    newsList = newsList.filter(articles => articles.title !== "[Removed]");
+    const newsHTML = newsList.map(news=>`
+    <div class="row news">
+                <div class="col-lg-4">
+                    <img class="news-img-size" src=${news.urlToImage} alt="">
+                </div>
+                <div class="col-lg-8">
+                    <h2>${news.title}</h2>
+                    <p>
+                        ${news.description}
+                    </p>
+                    <div>
+                        ${news.source.name} ${news.publishedAt} ${news.content}
+                    </div>
+                </div>
+            </div>
+    `).join('')
+
+    document.getElementById('news-board').innerHTML = newsHTML
+}
+
+getLatestNews();
+
+// 버튼들에 크릭이벤트 주기
+// 카테고리별 뉴스 가져오기
+// 그 뉴스 보여주기
