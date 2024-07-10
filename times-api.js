f0fe62ef74ba08a9ddc4631b8e889f662c5a9b63
etesong014 <etesong0114@gmail.com>
2024-07-10 21:12:32 +0900

    netlify를 위한 news api 수정 - 제공 api의 주소가 http여서 https로 변환해주는 과정을 거쳤는데 api의 주소 자체를 https로 수정함에 따라서 _redirects 파일 제거

--- times-api.js
+++ times-api.js
@@ -1,4 +1,4 @@
 export const KEY = {
         API_KEY: '',
-        API_URL: 'https://news-timesbysong.netlify.app/top-headlines'
+        API_URL: 'https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines'
     };
